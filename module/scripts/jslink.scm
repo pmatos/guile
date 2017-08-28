05c57a6a66904861bb760aea13dd3ad3117c5966
Ian Price <ianprice90@googlemail.com>
2017-08-28 14:08:31 +0100

    Update Copyright Headers
    * module/Makefile.am:
    * module/language/cps/compile-js.scm:
    * module/language/cps/spec.scm:
    * module/language/javascript.scm:
    * module/language/javascript/spec.scm:
    * module/language/js-il.scm:
    * module/language/js-il/compile-javascript.scm:
    * module/language/js-il/inlining.scm:
    * module/language/js-il/runtime.js: Update copyright headers

--- module/scripts/jslink.scm
+++ module/scripts/jslink.scm
@@ -1,3 +1,32 @@
+;;; jslink --- Link Together JS Modules
+
+;; Copyright 2017 Free Software Foundation, Inc.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public License
+;; as published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with this software; see the file COPYING.LESSER.  If
+;; not, write to the Free Software Foundation, Inc., 51 Franklin
+;; Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+;;; Author: Ian Price <ianprice90@gmail.com>
+
+;;; Commentary:
+
+;; Usage: jslink [ARGS]
+;;
+;; A command-line tool for linking together compiled JS modules.
+
+;;; Code:
+
 (define-module (scripts jslink)
   #:use-module (system base compile)
   #:use-module (system base language)
